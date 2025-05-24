fe99f46e4e1b13c48b22796dd04c7df447cd98c1
SuraSammour12 <surasammour2001@gmail.com>
2025-05-24 20:58:22 +0300

    Add files via upload

--- RemoveDuplicatesfromSortedArray.py
+++ RemoveDuplicatesfromSortedArray.py
@@ -1,3 +1,16 @@
 # 26. Remove Duplicates from Sorted Array
 # https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 
+class Solution(object):
+    def removeDuplicates(self, nums):
+        # Two Pointers
+        if not nums: # Empty list
+            return 0
+        i = 0 # this pointer for the last unique number, i start from the 1st number
+        for j in range(1,len(nums)):
+            if nums[j] != nums[i]: # find a unique number nums[j]
+               i+=1 # new place for the new unique num
+               nums[i]=nums[j]
+        return i+1 # number of unique elements; i is index-based (starts from 0), count starts from 1
+        
+        
